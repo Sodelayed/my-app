fd67f19f65ccfc05189b76d75dc76c3b5757677f
Ivanov Dmitriy <diver_space@mail.ruecho>
2023-09-14 20:55:49 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
-	const date = new Date();
-	const root = document.querySelector('#root');
-	const container = document.createElement('div');
-	container.className = 'App';
-	root.append(container);
-	const header = document.createElement('header');
-	header.className = 'App-header';
-	container.append(header);
-
-	const img = document.createElement('img');
-	img.src = logo;
-	img.className = 'App-logo';
-	img.alt = 'logo';
-	header.append(img);
-
-	const p = document.createElement('p');
-	p.textContent = 'Edit src/App.js and save to reload';
-	header.append(p);
-
-	const a = document.createElement('a');
-	a.className = 'App-link';
-	a.href = 'https://reactjs.org';
-	a.target = '_blank';
-	a.rel = 'noopener noreferrer';
-	a.textContent = 'Learn React';
-	header.append(a);
-
-	const year = document.createElement('p');
-	year.textContent = date.getFullYear();
-	header.append(year);
+	return (
+		<div className="App">
+			<output></output>
+		</div>
+	);
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 
-const root = document.createElement('div');
-root.id = 'root';
-App();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
